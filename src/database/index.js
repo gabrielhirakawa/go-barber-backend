a0f5b6044534697d6c10ed54a65158f95f851fe8
Gabriel Hirakawa <gabriel.souza30@fatec.sp.gov>
2019-10-14 20:01:27 -0300

    fist commit

---
+++ src/database/index.js
@@ -0,0 +1,36 @@
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
+
+import databaseconfig from '../config/database';
+
+const models = [User, File, Appointment];
+
+class Database{
+    constructor(){
+        this.init();
+        this.mongo();
+    }
+    // inicia conexao postgres
+    init(){
+        this.connection = new Sequelize(databaseconfig);
+
+        models
+        .map(model => model.init(this.connection))
+        .map(model => model.associate && model.associate(this.connection.models));
+        
+    }
+    // inicia conexao mongo
+    mongo(){
+        this.mongoConnection = mongoose.connect(
+            process.env.MONGO_URL,
+            { useNewUrlParser: true, useFindAndModify: true }
+        );
+    }
+
+}
+
+export default new Database();
